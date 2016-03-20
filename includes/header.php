2748d715acfcfd0e23236bdbbeef4a38a57742a5
Tom Wicks <tom@miln.co>
2016-03-20 12:19:14 +0000

    Added Menu

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<header>
+	<div class="grid-container">
+		<div class="col-4">
+			<h1>Mediapreview</h1>
+		</div>
+		<div class="col-6">
+			<nav>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">About</a></li>
+					<li><a href="">Tutorials</a></li>
+					<li><a href="">Movies</a></li>
+					<li><a href="">8mm Distress Kit</a></li>
+					<li><a href="">8mm Stock Footage</a></li>
+					<li><a class="link-btn-one" href="">Contact Us</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="style.css" rel="stylesheet" type="text/css">
+<title>MediaPreview &#8594; Home </title>
+</head>
+
+<body>
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/footer.php'; ?>
+</body>
+
+</html>
