7f18a4fe1cd11c307595873dd30ad1740cccf060
tomwicks <TomW@Pans-MacBook-Pro.local>
2016-08-13 15:41:48 +0100

    Contact Form Working

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 	<?php include 'includes/header.php'; ?>
-	<article class="about">
+	<article class="contact">
 		<section class="hero">
 			<img class="lazy" data-original="images/bg-1.jpg">
 			<div class="grid-container">
@@ -21,81 +21,45 @@
 		<section class="longform">
 			<div class="grid-container">
 				<div class="row">
-					<div class="col-4">
-						<p>Mediapreview's Andy Wicks and his associates have been making videos and screen media products for commercial, public sector and community clients for the past 30 years.</p>
-					</div> <!-- col-4 -->
-					<div class="col-6">		
-						<form action="email-form.php" method="POST">
-						<p>Name</p> <input type="text" name="name">
-						<p>Email</p> <input type="text" name="email">
-						<p>Phone</p> <input type="text" name="phone">
+					<div class="col-3">
+						<h4>Work With Us</h4>
+						<p>Have a project in mind? Use the contact form or get in touch:</p>
+						<a href="mailto:andy@mediapreview.net" target="_top">andy@mediapreview.net</a>
+						<a href="tel:+44 (0)7901 915651" target="_top">+44 (0)7901 915651</a>
+					</div> <!-- col-3 -->
+					<div class="col-3">
+						<h4>Stock Footage Enquiries</h4>
+						<p>Want more information about our 8mm products? Email us here</p>
+						<a href="mailto:admin@mediapreview.net" target="_top">admin@mediapreview.net</a>
+					</div> <!-- col-3 -->
+					<div class="col-6">
+						<h4>Contact Form</h4>		
+						<form id="ajax-contact" method="post" action="mailer.php">
+						    <div class="field form-half">
+						        <label for="name">Name:</label>
+						        <input type="text" id="name" name="name" required>
+						    </div>
 
-						<p>Request Phone Call:</p>
-						Yes:<input type="checkbox" value="Yes" name="call"><br />
-						No:<input type="checkbox" value="No" name="call"><br />
+						    <div class="field form-half">
+						        <label for="email">Email:</label>
+						        <input type="email" id="email" name="email" required>
+						    </div>
 
-						<p>Website</p> <input type="text" name="website">
+						    <div class="field">
+						        <label for="message">Message:</label>
+						        <textarea rows="8" id="message" name="message" required></textarea>
+						    </div>
 
-						<p>Priority</p>
-						<select name="priority" size="1">
-						<option value="Low">Low</option>
-						<option value="Normal">Normal</option>
-						<option value="High">High</option>
-						<option value="Emergency">Emergency</option>
-						</select>
-						<br />
+						    <div id="form-messages"></div>
 
-						<p>Type</p>
-						<select name="type" size="1">
-						<option value="update">Website Update</option>
-						<option value="change">Information Change</option>
-						<option value="addition">Information Addition</option>
-						<option value="new">New Products</option>
-						</select>
-						<br />
-
-						<p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
-						<input type="submit" value="Send"><input type="reset" value="Clear">
+						    <div class="field">
+						        <button type="submit">Send</button>
+						    </div>
 						</form>
 					</div> <!-- col-6 -->
 				</div> <!-- row -->
 			</div> <!-- grid-container -->
 		</section> <!-- longform -->
-		<section class="testimonials">
-			<div class="swiper-container">
-	        <div class="swiper-wrapper">
-	            <div class="swiper-slide">
-	            	<div class="grid-container">
-	            		<div class="col-8">
-	            			<h2>"Mediapreview and Longley Farm have worked together for nearly twenty years now. They can put together all that is needed from idea to edit; from script to soundtrack. They are totally reliable and you can be sure of a good result."</h2>
-	            			<h5>Jim Dickinson, Longley Farm</h5>
-	            		</div>
-	            	</div>
-	            </div>
-	            <div class="swiper-slide">
-	            	<div class="grid-container">
-	            		<div class="col-8">
-	            			<h2>"Mediapreview are a highly professional company who marry first class technical skills to a ready understanding  of a brief, and a passion for getting behind a subject."</h2>
-	            			<h5>Nancy Barrett, Creative Scene</h5>
-	            		</div>
-	            	</div>
-	            </div>
-	            <div class="swiper-slide">
-	            	<div class="grid-container">
-	            		<div class="col-8">
-	            			<h2>Mediapreview keep leaving their chewing gum on the side of the table and it's really annoying.</h2>
-	            			<h5>Tom Wicks, Miln</h5>
-	            		</div>
-	            	</div>
-	            </div>
-	        </div>
-	        <!-- Add Pagination -->
-	        <div class="swiper-pagination"></div>
-	        <!-- Add Arrows -->
-	        <div class="swiper-button-next"></div>
-	        <div class="swiper-button-prev"></div>
-	    </div>
-		</section>
 	</article> <!-- about -->
 	<?php include 'includes/footer.php'; ?>
 </body>
--- movies.php
+++ movies.php
@@ -32,7 +32,7 @@
 					<div class="col-6">
 						<div class="movie-picker-item">
 							<div class="image"><iframe src="//player.vimeo.com/video/144060482?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
-							<h4>TRAIN of THOUGHT - The Inward Journey</h4>
+							<h4>Train of Thought - The Inward Journey</h4>
 							<p>Commissioned by Creative Scene - day dreaming passengers share their
 secret thoughts and Transpennine staff reveal behind-the-scene truths on
 the journey from Huddersfield to Leeds.</p>
@@ -45,7 +45,7 @@ the journey from Huddersfield to Leeds.</p>
 						<div class="movie-picker-item">
 							<div class="image"><iframe src="//player.vimeo.com/video/139607076?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
 							<h4>Longley Farm - New Wind Turbine</h4>
-							<p>High up in the hills above Holmfirth, it's time to replace UK's first commercial wind turbine.</p>
+							<p>High up in the hills above Holmfirth, it's time to replace the UK's first commercial wind turbine.</p>
 							<h5>04 Mins<span>2015</span></h5>
 						</div> <!-- movie-picker -->
 					</div> <!-- col-4 -->
@@ -71,26 +71,92 @@ for the Trinity Leeds retail space, from conception to installation.</p>
 				<div class="row">
 					<div class="col-4">
 						<div class="movie-picker-item">
-							<div class="image"></div>
-							<h2>Train of Thought</h2>
-							<p>The train of thought or track of thought refers to the interconnection in the sequence of ideas.</p>
-							<h5>17 minutes</h5>
+							<div class="image"><iframe src="//player.vimeo.com/video/178164456?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+							<h4>Let’s Fly Away - Trailer</h4>
+							<p>Shabang's Captain Roland Groove and his trusty assistant Reenie on a magical musical flight like no other.</p>
+							<h5>4 Mins<span>2016</span></h5>
 						</div> <!-- movie-picker -->
+					</div> <!-- col-4 -->
+				
+				
+					<div class="col-4">
+						<div class="movie-picker-item">
+							<div class="image"><iframe src="//player.vimeo.com/video/178158132?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+							<h4>Look at the Stars - World Down Syndrome Day 2016</h4>
+							<p>Shabang's Up Club celebrate WDSD2016.</p>
+							<h5>03 Mins<span>2016</span></h5>
+						</div> <!-- movie-picker -->
+						</div> <!-- col-4 -->
+
+					<div class="col-4">
+						<div class="movie-picker-item">
+							<div class="image"><iframe src="//player.vimeo.com/video/178175186?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+							<h4>Hot Against the Cold - Promo</h4>
+							<p>Promo for local band, Hot Against the Cold, shot in the studio as they recorded
+                            their new EP; Excuse Me Mister.</p>
+							<h5>12 Mins<span>2012</span></h5>
+							</div> <!-- movie-picker -->
+					</div> <!-- col-4 -->
+
+			
+					</div> <!-- row -->
+				<div class="row">
+
+					<div class="col-4">
+						<div class="movie-picker-item">
+							<div class="image"><iframe src="//player.vimeo.com/video/178158133?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+							<h4>Chromosomes R Us - World Down Syndrome Day 2014</h4>
+							<p>Made with Shabang Inclusive Learning, Actors with Down Syndrome show how
+Trisomy 21 occurs. Nearly 50,00 You Tube hits!</p>
+							<h5>06 Mins<span>2014</span></h5>
+							</div> <!-- movie-picker -->
 					</div> <!-- col-4 -->
 					<div class="col-4">
 						<div class="movie-picker-item">
-							<div class="image"></div>
-							<h2>Train of Thought</h2>
-							<p>The train of thought or track of thought refers to the interconnection in the sequence of ideas.</p>
-							<h5>17 minutes</h5>
-						</div> <!-- movie-picker -->
+							<div class="image"><iframe src="//player.vimeo.com/video/178180855?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+							<h4>Miss Lockwood - The Lady Behind the Lens</h4>
+							<p>First part of a 35 minute, tribute to a pioneering and prolific local film maker, Miss Kathleen Lockwood. Produced for Holmfirth Film Festival.</p>
+							<h5>15 Mins<span>2011</span></h5>
+							</div> <!-- movie-picker -->
 					</div> <!-- col-4 -->
 					<div class="col-4">
 						<div class="movie-picker-item">
-							<div class="image"></div>
-							<h2>Train of Thought</h2>
-							<p>The train of thought or track of thought refers to the interconnection in the sequence of ideas.</p>
-							<h5>17 minutes</h5>
+							<div class="image"><iframe src="//player.vimeo.com/video/178194169?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+							<h4>Primary Focus</h4>
+							<p>An internal communication for Co-operative Retail Logistics (CRL), the transport
+and distribution arm of The Cooperative Group.</p>
+							<h5>10 Mins<span>2008</span></h5>
+						</div> <!-- movie-picker -->
+					</div> <!-- col-4 -->
+
+							</div> <!-- row -->
+				<div class="row">
+
+					<div class="col-4">
+						<div class="movie-picker-item">
+							<div class="image"><iframe src="//player.vimeo.com/video/178194369?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+							<h4>Our Sporting Greats</h4>
+							<p>Made for Kirklees Met. Council, Our Sporting Greats profiles sporting heros
+from the area.</p>
+							<h5>30 Mins<span>2008</span></h5>
+							</div> <!-- movie-picker -->
+					</div> <!-- col-4 -->
+					<div class="col-4">
+						<div class="movie-picker-item">
+							<div class="image"><iframe src="//player.vimeo.com/video/82631450?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+							<h4>Late Nite Session</h4>
+							<p>Shot on 8mm cine, Late Nite Session tells the tale of a 1980's reggae blues party.
+Over 17,000 You Tube hits.</p>
+							<h5>10 Mins<span>2002</span></h5>
+							</div> <!-- movie-picker -->
+					</div> <!-- col-4 -->
+					<div class="col-4">
+						<div class="movie-picker-item">
+							<div class="image"><iframe src="//player.vimeo.com/video/178355486?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" width="100%" height="100%" frameborder="0" class="landscape pri" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+							<h4>Spoof Reel</h4>
+							<p>Early After Effects work out using vintage clips from the Internet Archive. Spoof Reel was designed
+as a e-mail calling card to attract new freelance business.</p>
+							<h5>03 Mins<span>2005</span></h5>
 						</div> <!-- movie-picker -->
 					</div> <!-- col-4 -->
 				</div> <!-- row -->
