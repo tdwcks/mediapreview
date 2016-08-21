cf48829bd9e96408dc72c78a6f825323ab5e056e
tomwicks <TomW@Pans-MBP.home>
2016-08-21 15:23:25 +0100

    Testimonials Mobile Working

--- production.php
+++ production.php
@@ -21,6 +21,14 @@
 		<section class="longform">
 			<div class="grid-container">
 				<div class="row">
+					<div class="col-6">
+						<p class="large">Mediapreview’s Andy Wicks and his associates have been making videos and screen media products for commercial, public sector and community clients for the past 30 years.</p>
+						<p>We drill down find out what the client really needs and make sure it gets delivered to the highest standard, on time and on budget.</p>
+						<p>On the production side, using state of the art 4k DSLR technology, we work unobtrusively, striking the balance between putting interviewees at ease and shooting useable material that shows everyone in the best light - literally and metaphorically! <p>We have extensive experience of shooting and editing green screen.</p>
+						<p>Post - production wise we work creatively and speedily - finding the narrative, assembling films that sparkle, flow seamlessly with a light touch and bring out the best in the participants.</p>
+						<p>Then we know how to get the films seen - on websites, on demand, social and DVD.</p>
+						<p>If you have a project in mind please get in touch.</p>
+					</div> <!-- col-6 -->
 					<div class="col-4">
 						<div class="testimonials">
 					        <div class="swiper-controls">
@@ -28,7 +36,6 @@
 						        <div class="swiper-button-prev"></div>
 					        </div>
 					        <h4>Testimonials</h4>
-						
 							<div class="swiper-container">
 						        <div class="swiper-wrapper">
 						            <div class="swiper-slide">
@@ -51,14 +58,7 @@
 						    </div>
 						</div> <!-- Testimonials -->
 					</div> <!-- col-4 -->
-					<div class="col-6">
-						<p class="large">Mediapreview’s Andy Wicks and his associates have been making videos and screen media products for commercial, public sector and community clients for the past 30 years.</p>
-						<p>We drill down find out what the client really needs and make sure it gets delivered to the highest standard, on time and on budget.</p>
-						<p>On the production side, using state of the art 4k DSLR technology, we work unobtrusively, striking the balance between putting interviewees at ease and shooting useable material that shows everyone in the best light - literally and metaphorically! <p>We have extensive experience of shooting and editing green screen.</p>
-						<p>Post - production wise we work creatively and speedily - finding the narrative, assembling films that sparkle, flow seamlessly with a light touch and bring out the best in the participants.</p>
-						<p>Then we know how to get the films seen - on websites, on demand, social and DVD.</p>
-						<p>If you have a project in mind please get in touch.</p>
-					</div> <!-- col-6 -->
+					
 				</div> <!-- row -->
 			</div> <!-- grid-container -->
 		</section> <!-- longform -->
